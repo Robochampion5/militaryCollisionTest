98dc305e6fd6f115c02b2e0f95ef69281ba7c0d3
Adarsh Singh <70940310+Robochampion5@users.noreply.github.com>
2022-02-19 17:08:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup(){
 function draw(){
   background(0,0,0);
   bullet.velocityX = speed;
-  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+  if(hasCollided(bullet,wall)){
     bullet.velocityX=0;
     var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
     if(damage>7){
