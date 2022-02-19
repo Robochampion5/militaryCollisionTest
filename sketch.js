a16eff3de0cec9da30f3a43a56156f90e875acbb
Adarsh Singh <70940310+Robochampion5@users.noreply.github.com>
2022-02-19 17:13:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,10 +16,10 @@ function draw(){
   if(hasCollided(bullet,wall)){
     bullet.velocityX=0;
     var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
-    if(damage>7){
+    if(damage>10){
       wall.shapeColor=color(255,0,0);
     }
-    if(damage<7){
+    if(damage<10){
       wall.shapeColor=color(0,255,0);
     }
   }
